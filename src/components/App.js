d8010436314cb2bd28d6f9dafd254b24328541bc
Gilbert Malova <malovagilbert@gmail.com>
2022-12-28 13:21:13 +0300

    Initialized app with create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import '../Styles/App.css';
+import Cards from "./cards";
+
+const App = () => {
+
+  return (
+    <div>
+      <Cards/>
+    </div>
+  )
+};
+
+export default App;
---
+++ src/components/cards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState , useEffect } from "react";
+
+const Cards = () => {
+  const [cards, setCards] = useState([]);
+
+  return (
+    <div>
+      <h1>Card game</h1>
+    </div>
+  )
+}
+
+export default Cards;
